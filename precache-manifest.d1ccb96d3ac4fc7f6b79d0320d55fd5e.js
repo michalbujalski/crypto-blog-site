49c94fc7527ee4a052d9feafb4b252addfc47a3b
Michal Bujalski <bujalski.michal@gmail.com>
2020-03-19 22:07:26 +0100

    1.0.0 version

---
+++ precache-manifest.d1ccb96d3ac4fc7f6b79d0320d55fd5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8a1d131925ce109fe2bfa098261881",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f6fe415e15a28494fc5",
+    "url": "/static/css/main.b49d9435.chunk.css"
+  },
+  {
+    "revision": "0bcf5880b067ab1eb6ce",
+    "url": "/static/js/2.38550598.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.38550598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f6fe415e15a28494fc5",
+    "url": "/static/js/main.f35bb59c.chunk.js"
+  },
+  {
+    "revision": "bd38d7a48da7a9a86974",
+    "url": "/static/js/runtime-main.34e7e66b.js"
+  }
+]);
